702edb606a6490d4813b207ad3c2ee9f04326203
Anderson Peñaloza <designeromega@gmail.com>
2023-12-06 16:27:17 +0100

    chore: :lipstick: add loading component
    Signed-off-by: Anderson Peñaloza <designeromega@gmail.com>

---
+++ src/app/shared/ui/components/loading/loading.component.spec.ts
@@ -0,0 +1,27 @@
+import {
+  ComponentFixture,
+  TestBed,
+  waitForAsync
+} from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { LoadingComponent } from './loading.component';
+
+describe('LoadingComponent', () => {
+  let component: LoadingComponent;
+  let fixture: ComponentFixture<LoadingComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(LoadingComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
